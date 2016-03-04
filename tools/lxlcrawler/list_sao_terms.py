d34f35f130b9c915415fa23ae21e5fcd5de4eb4e
Niklas Lindström <lindstream@gmail.com>
2016-03-04 14:31:08 +0100

    Update crawler example

--- tools/lxlcrawler/list_sao_terms.py
+++ tools/lxlcrawler/list_sao_terms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import lxlcrawler
 
-i = 0
-for item in lxlcrawler.crawl("https://id.kb.se/find?q=*&limit=200&inScheme.@id=https://id.kb.se/term/sao"):
-    i += 1
-    print("[%s]" % i, item['@id'], item.get('prefLabel'))
+start_url = "https://id.kb.se/find?q=*&limit=200&inScheme.@id=https://id.kb.se/term/sao"
+
+for i, item in enumerate(lxlcrawler.crawl(start_url)):
+    print("[{}] {}, {}".format(i + 1, item['@id'], item.get('prefLabel')))
