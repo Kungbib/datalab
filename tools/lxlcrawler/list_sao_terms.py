35966403b243f7ac7effa0bc69e2938b9324278b
Niklas Lindström <lindstream@gmail.com>
2021-01-23 21:08:52 +0100

    Change limit in lxlcrawler sao example script

--- tools/lxlcrawler/list_sao_terms.py
+++ tools/lxlcrawler/list_sao_terms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import lxlcrawler
 
-start_url = "https://id.kb.se/find?q=*&limit=200&inScheme.@id=https://id.kb.se/term/sao"
+start_url = "https://id.kb.se/find?q=*&_limit=1024&inScheme.@id=https://id.kb.se/term/sao"
 
 for i, item in enumerate(lxlcrawler.crawl(start_url)):
     print("[{}] {}, {}".format(i + 1, item['@id'], item.get('prefLabel')))
